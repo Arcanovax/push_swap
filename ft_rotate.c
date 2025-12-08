bed45ac7c2b94c7dccfdf1cf1cf51379edc0a803
MICKBOUT <matisse.boutte@gmail.com>
2025-12-08 17:41:33 +0100

    factorisation ft_rotate

--- ft_rotate.c
+++ ft_rotate.c
@@ -6,69 +6,53 @@
 /*   By: mboutte <mboutte@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 14:39:58 by mboutte           #+#    #+#             */
-/*   Updated: 2025/12/08 16:52:38 by mboutte          ###   ########.fr       */
+/*   Updated: 2025/12/08 17:40:33 by mboutte          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_rotate_a(t_stack **a)
+static int ft_rotate(t_stack **head)
 {
 	t_stack	*current_node;
 
-	if (!a || !*a || !(*a)->next)
-		return (0);
-	current_node = *a;
+	if (!head || !*head || !(*head)->next)
+		return (-1);
+	current_node = *head;
 	while (current_node->next != NULL)
 		current_node = current_node->next;
-	current_node->next = *a;
-	*a = (*a)->next;
+	current_node->next = *head;
+	*head = (*head)->next;
 	current_node->next->next = NULL;
-	if (write(1, "ra\n", 3) == -1)
+	return (0);
+}
+int	ft_rotate_a(t_stack **a)
+{
+	if (ft_rotate(a) == -1 || write(1, "ra\n", 3) == -1)
 		return (-1);
 	return (0);
 }
 
 int	ft_rotate_b(t_stack **b)
 {
-	t_stack	*current_node;
-
-	if (!b || !*b || !(*b)->next)
-		return (0);
-	current_node = *b;
-	while (current_node->next != NULL)
-		current_node = current_node->next;
-	current_node->next = *b;
-	*b = (*b)->next;
-	current_node->next->next = NULL;
-	if (write(1, "rb\n", 3) == -1)
+	if (ft_rotate(b) == -1 || write(1, "rb\n", 3) == -1)
 		return (-1);
 	return (0);
 }
 int	ft_rotate_ab(t_stack **a, t_stack **b)
 {
-	t_stack	*current_node;
-	
-	if (a && *a && (*a)->next && b && *b && (*b)->next)
+	int	res_a;
+	int	res_b;
+
+	res_a = ft_rotate(a);
+	res_b = ft_rotate(b);
+	if (res_a == 0 && res_b == 0)
+		return (write(1, "rr\n", 3));
+	else if (res_a == 0)
 	{
-		current_node = *b;
-		while (current_node->next != NULL)
-			current_node = current_node->next;
-		current_node->next = *b;
-		*b = (*b)->next;
-		current_node->next->next = NULL;
-		current_node = *a;
-		while (current_node->next != NULL)
-			current_node = current_node->next;
-		current_node->next = *a;
-		*a = (*a)->next;
-		current_node->next->next = NULL;	
-		if (write(1, "rr\n", 3) == -1)
-			return (-1);
-		return (0);
+		return (write(1, "ra\n", 3) >= 0);
 	}
-	if (a && *a && (*a)->next)
-		return (ft_rotate_a(a));
-	else
-		return (ft_rotate_b(b));
+	else if (res_b == 0)
+		return write(1, "rb\n", 3);
+	return (0);
 }
