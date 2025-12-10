d113354f5502e191b5b310aae518bcbfcd46f315
MICKBOUT <matisse.boutte@gmail.com>
2025-12-10 15:54:45 +0100

    remove commentaire from  randix

--- ft_randix.c
+++ ft_randix.c
@@ -6,41 +6,11 @@
 /*   By: mboutte <mboutte@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 16:16:27 by mboutte           #+#    #+#             */
-/*   Updated: 2025/12/10 12:44:45 by mboutte          ###   ########.fr       */
+/*   Updated: 2025/12/10 15:54:24 by mboutte          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-// void	ft_set_unsigned(t_stack *stack)
-// {
-// 	int last_before;
-
-// 	if (!stack || !stack->head)
-// 		return ;
-// 	last_before = stack->tails->value;
-// 	while (stack->head->value != last_before)
-// 	{
-// 		stack->head->value -= INT_MIN;
-// 		ft_rotate_a(stack);
-// 	}
-// 	stack->head->value -= INT_MIN;
-// }
-
-// void	ft_unset_unsigned(t_stack *stack)
-// {
-// 	int last_before;
-
-// 	if (!stack || !stack->head)
-// 		return ;
-// 	last_before = stack->tails->value;
-// 	while (stack->head->value != last_before)
-// 	{
-// 		stack->head->value += INT_MIN;
-// 		ft_rotate_a(stack);
-// 	}
-// 	stack->head->value += INT_MIN;
-// }
 
 int	ft_randinx(t_stack *a, t_stack *b)
 {
@@ -48,7 +18,6 @@ int	ft_randinx(t_stack *a, t_stack *b)
 	int bit;
 
 	bit = 0;
-	// ft_set_unsigned(a);
 	while (bit < 32)
 	{
 		size = a->size;
@@ -63,7 +32,5 @@ int	ft_randinx(t_stack *a, t_stack *b)
 			ft_push_a(a, b);
 		bit++;
 	}
-	// ft_unset_unsigned(a);
-	// ft_rotate_a(a);
 	return (0);
 }
