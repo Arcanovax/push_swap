72ee97baeea5d1bd0d6ee193ee1c95dcbd1b9fee
MICKBOUT <matisse.boutte@gmail.com>
2025-12-10 15:51:55 +0100

    fix compute disorder

--- compute_disorder.c
+++ compute_disorder.c
@@ -6,7 +6,7 @@
 /*   By: mboutte <mboutte@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 17:01:04 by mthetcha          #+#    #+#             */
-/*   Updated: 2025/12/10 15:38:46 by mboutte          ###   ########.fr       */
+/*   Updated: 2025/12/10 15:51:37 by mboutte          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,43 @@ double	compute_disorder(t_stack *stack)
 	t_node	*node;
 
 	mistakes = 0;
-	total_pairs = (double)stack->size;
+	total_pairs = 0;
 	head = stack->head;
 	node = head->next;
-	while (head->next != NULL)
+	while (head != NULL)
 	{
 		node = head->next;
 		while (node != NULL)
 		{
+			total_pairs++;
 			if (head->value > node->value)
 				mistakes++;
-			node = node->value;
+			node = node->next;
 		}
 		head = head->next;
 	}
 	return ((double)mistakes / (double)total_pairs);
 }
+static void	ft_print_stack(t_stack *stk)
+{
+	t_node *node = stk->head;
+	__builtin_printf("stack\nhead: % 2d|%p\ntails:%2d|%p\nsize:%3d|\n", stk->head->value, stk->head, stk->tails->value, stk->tails, stk->size);
+	while (node) {
+		__builtin_printf("prev:%10p|node_a:%2d|%p|next:%-10p\n",node->prev, node->value, node, node->next);
+		node = node->next;
+	}
+}
 
+static void ft_free_stack(t_stack *stk)
+{
+	t_node *node = stk->head;
+	t_node *next;
+	
+	while (node)
+	{
+		next = node->next;
+		free(node);
+		node = next;
+	}
+	free(stk);
+}
