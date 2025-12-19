b4c9d3a4a319fd05224f8c23dd4302098156597b
MICKBOUT <matisse.boutte@gmail.com>
2025-12-19 14:52:09 +0100

    fix atoi

--- .obj/ft_free_exit.d
+++
@@ -1,5 +0,0 @@
-.obj/ft_free_exit.o: ft_free_exit.c push_swap.h push_swap_common.h
-
-push_swap.h:
-
-push_swap_common.h:
--- .obj/ft_process.d
+++
@@ -1,5 +0,0 @@
-.obj/ft_process.o: ft_process.c push_swap.h push_swap_common.h
-
-push_swap.h:
-
-push_swap_common.h:
